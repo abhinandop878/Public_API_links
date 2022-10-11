370daf78bb7d4cb965f3bb1e8636f786376d03cc
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-11 22:21:42 +0530

    Api

--- src/app/public-api/public-api.component.ts
+++ src/app/public-api/public-api.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-public-api',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicApiComponent implements OnInit {
 
-  constructor() { }
-publicData={}
+  constructor(private myapi:ApiService) { 
+    this.fetchData()
+  }
+  fetchData=()=>{
+    this.myapi.viewPublicData().subscribe((data)=>{
+      this.publicData=data
+    })
+  }
+publicData:any={}
   ngOnInit(): void {
   }
 
